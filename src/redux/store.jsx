731ca8ecc41b00f31e58dcc39b21ce14534f461d
Shariar2006 <shariarnafis86@gmail.com>
2024-03-13 19:35:51 +0600

    add redux logger with redux middleware

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import CounterReducer from './features/counter/counterSlice'
+import logger from 'redux-logger'
 
 export const store = configureStore({
   reducer: {
     counter: CounterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(logger),
 })
